ccc1665af0dd721262790526cb2f4978f9b510f4
redBM <bors996@gmail.com>
2025-01-31 15:50:50 +0100

    1.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'taskManager.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/api/system-info', async (req, res) => {
